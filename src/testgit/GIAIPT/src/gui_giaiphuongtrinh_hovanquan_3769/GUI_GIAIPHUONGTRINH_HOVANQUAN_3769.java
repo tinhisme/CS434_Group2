ac43b965d929248dd6d61473394b0b99547a81f6
chungDN <100787827+chungDN@users.noreply.github.com>
2022-03-03 11:31:36 +0700

    Add files via upload

---
+++ src/testgit/GIAIPT/src/gui_giaiphuongtrinh_hovanquan_3769/GUI_GIAIPHUONGTRINH_HOVANQUAN_3769.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui_giaiphuongtrinh_hovanquan_3769;
+
+/**
+ *
+ * @author Hồ Quân
+ */
+public class GUI_GIAIPHUONGTRINH_HOVANQUAN_3769 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        frm_Giaiphuongtrinh c = new frm_Giaiphuongtrinh();
+        c.show();
+    }
+    
+}
